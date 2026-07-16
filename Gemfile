dceee7b3a4490ef78e7e4bec62273913920b469f
umineko15 <saikidou15918@gmail.com>
2026-07-16 15:10:53 +0900

    情報の保存

--- Gemfile
+++ Gemfile
@@ -80,4 +80,5 @@ group :production do
 end
 
 gem 'active_hash'
-
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+
+  has_one_attached :image
+
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  validates :description, :text, presence: true
+  validates :description, presence: true
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
